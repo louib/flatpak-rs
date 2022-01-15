c5ba0cbf87226b03f76a2579b526785f87a6d535
louib <code@louib.net>
2022-01-14 23:28:55 -0500

    Publish as both flatpak and flatpak-rs

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Publish a new version of the library.
 
+SCRIPT_DIR=$(realpath "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_DIR")
+
 set -e
 
 die() { echo "🔥 Error: $*" 1>&2; exit 1; }
@@ -11,4 +14,15 @@ fi
 
 cargo build --release
 cargo publish
-echo "📦 Published the package on crates.io."
+echo "📦 Published the flatpak-rs package on crates.io."
+
+# FIXME this is hackish and a better way would be to properly
+# parse the TOML file. But for now this does the trick.
+sed -i 's/name = "flatpak-rs"/name = "flatpak"/g' "$SCRIPT_DIR/../Cargo.toml"
+
+cargo build --release
+cargo publish
+echo "📦 Published the flatpak package on crates.io."
+
+# Revert the hack from above.
+sed -i 's/name = "flatpak"/name = "flatpak-rs"/g' "$SCRIPT_DIR/../Cargo.toml"
