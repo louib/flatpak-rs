93812ee49534b8d486caf3b4a14ec4850f5007a2
louib <code@louib.net>
2022-01-18 02:00:49 -0500

    Add basic ArchiveType module

---
+++ src/archive.rs
@@ -0,0 +1,40 @@
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+pub const RPM: &str = "rpm";
+pub const TAR: &str = "tar";
+pub const TAR_GZIP: &str = "tar-gzip";
+pub const TAR_COMPRESS: &str = "tar-compress";
+pub const TAR_BZIP2: &str = "tar-bzip2";
+pub const TAR_LZIP: &str = "tar-lzip";
+pub const TAR_LZMA: &str = "tar-lzma";
+pub const TAR_LZOP: &str = "tar-lzop";
+pub const TAR_XZ: &str = "tar-xz";
+pub const ZIP: &str = "zip";
+pub const SEVENZIP: &str = "7z";
+
+#[derive(Clone)]
+#[derive(Deserialize)]
+#[derive(Debug)]
+#[derive(Hash)]
+pub enum ArchiveType {
+    Rpm,
+}
+
+impl Default for ArchiveType {
+    fn default() -> Self {
+        ArchiveType::Rpm
+    }
+}
+impl ArchiveType {
+    pub fn to_string(&self) -> String {
+        match &self {
+            Rpm => RPM.to_string(),
+        }
+    }
+    pub fn from_string(archive_type: &str) -> Result<ArchiveType, String> {
+        if archive_type == RPM {
+            return Ok(ArchiveType::Rpm);
+        }
+        Err(format!("Invalid archive type {}.", archive_type))
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+pub mod archive;
 pub mod application;
 pub mod build_system;
 pub mod filename;
