817c1399779b47a8f37ad6c1c450a510656b65dd
louib <code@louib.net>
2022-01-23 13:08:55 -0500

    Add all archive types

--- src/archive.rs
+++ src/archive.rs
@@ -18,6 +18,16 @@ pub const SEVENZIP: &str = "7z";
 #[derive(Hash)]
 pub enum ArchiveType {
     Rpm,
+    Tar,
+    TarGzip,
+    TarCompress,
+    TarBzip2,
+    TarLzip,
+    TarLzma,
+    TarLzop,
+    TarXz,
+    Zip,
+    SevenZip,
 }
 
 impl Default for ArchiveType {
@@ -29,11 +39,55 @@ impl ArchiveType {
     pub fn to_string(&self) -> String {
         match &self {
             ArchiveType::Rpm => RPM.to_string(),
+            ArchiveType::SevenZip => SEVENZIP.to_string(),
+            ArchiveType::Tar => TAR.to_string(),
+            ArchiveType::TarGzip => TAR_GZIP.to_string(),
+            ArchiveType::TarCompress => TAR_COMPRESS.to_string(),
+            ArchiveType::TarBzip2 => TAR_BZIP2.to_string(),
+            ArchiveType::TarLzip => TAR_LZIP.to_string(),
+            ArchiveType::TarLzma => TAR_LZMA.to_string(),
+            ArchiveType::TarLzop => TAR_LZOP.to_string(),
+            ArchiveType::TarXz => TAR_XZ.to_string(),
+            ArchiveType::Zip => ZIP.to_string(),
+            ArchiveType::SevenZip => SEVENZIP.to_string(),
         }
     }
     pub fn from_string(archive_type: &str) -> Result<ArchiveType, String> {
         if archive_type == RPM {
             return Ok(ArchiveType::Rpm);
+        }
+        if archive_type == SEVENZIP {
+            return Ok(ArchiveType::SevenZip);
+        }
+        if archive_type == ZIP {
+            return Ok(ArchiveType::Zip);
+        }
+        if archive_type == TAR {
+            return Ok(ArchiveType::Tar);
+        }
+        if archive_type == TAR_XZ {
+            return Ok(ArchiveType::TarXz);
+        }
+        if archive_type == TAR_LZOP {
+            return Ok(ArchiveType::TarLzop);
+        }
+        if archive_type == TAR_COMPRESS {
+            return Ok(ArchiveType::TarCompress);
+        }
+        if archive_type == TAR_BZIP2 {
+            return Ok(ArchiveType::TarBzip2);
+        }
+        if archive_type == TAR_GZIP {
+            return Ok(ArchiveType::TarGzip);
+        }
+        if archive_type == TAR_LZIP {
+            return Ok(ArchiveType::TarLzip);
+        }
+        if archive_type == TAR_LZMA {
+            return Ok(ArchiveType::TarLzma);
+        }
+        if archive_type == TAR_LZOP {
+            return Ok(ArchiveType::TarLzop);
         }
         Err(format!("Invalid archive type {}.", archive_type))
     }
