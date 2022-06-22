357616ae0db2e44f15a9a3209f4efa23af6d644f
louib <code@louib.net>
2022-06-22 10:04:32 -0400

    Add the standard derives for new enum

--- src/manifest_type.rs
+++ src/manifest_type.rs
@@ -1,3 +1,10 @@
+use serde::{Deserialize, Serialize};
+
+/// All the Flatpak manifest types supported.
+#[derive(Clone)]
+#[derive(Deserialize)]
+#[derive(Serialize)]
+#[derive(Debug)]
 pub enum FlatpakManifestType {
     Application,
     Module,
