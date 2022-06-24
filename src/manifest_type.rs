033caec9d0a0495caa4de06acdd1f56b492f66ad
louib <code@louib.net>
2022-06-23 20:43:33 -0400

    Add ManifestType parsing and dumping

--- src/manifest_type.rs
+++ src/manifest_type.rs
@@ -1,5 +1,9 @@
 use serde::{Deserialize, Serialize};
 
+pub const APPLICATION: &str = "application";
+pub const MODULE: &str = "module";
+pub const SOURCE: &str = "source";
+
 /// All the Flatpak manifest types supported.
 #[derive(Clone)]
 #[derive(Deserialize)]
@@ -9,4 +13,26 @@ pub enum FlatpakManifestType {
     Application,
     Module,
     Source,
+}
+impl FlatpakManifestType {
+    pub fn to_string(&self) -> String {
+        match &self {
+            FlatpakManifestType::Application => APPLICATION.to_string(),
+            FlatpakManifestType::Module => MODULE.to_string(),
+            FlatpakManifestType::Source => SOURCE.to_string(),
+        }
+    }
+
+    pub fn from_string(manifest_type: &str) -> Result<FlatpakManifestType, String> {
+        if manifest_type == APPLICATION {
+            return Ok(FlatpakManifestType::Application);
+        }
+        if manifest_type == MODULE {
+            return Ok(FlatpakManifestType::Module);
+        }
+        if manifest_type == SOURCE {
+            return Ok(FlatpakManifestType::Source);
+        }
+        Err(format!("Invalid manifest type {}.", manifest_type))
+    }
 }
